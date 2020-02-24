8b4499dcaa590cd9ce95cb024ccef73b3a6cd4c7
injecti0n <rootgebruiker@gmail.com>
2020-02-24 15:45:22 +0100

    updated

--- JAVASCRIPT/String_StringMethods.js
+++ JAVASCRIPT/String_StringMethods.js
@@ -10,11 +10,11 @@ console.log(myConsole)
 
 var ShowInConsole;
 ShowInConsole = Msg.slice(21,40);
-ShowInConsole = Msg.slice(-21,-1)
-ShowInConsole = Msg.slice(7)
+ShowInConsole = Msg.slice(-21,-1);
+ShowInConsole = Msg.slice(7);
 ShowInConsole = Msg.substring(21,30); // substring() is similar to slice() but cannot accept negative indexes.
-ShowInConsole = Msg.substr(21,40) // method extract a part of a string and returns the extracted parts in a new string
-ShowInConsole = Msg.replace("porro","Morro")
+ShowInConsole = Msg.substr(21,40); // method extract a part of a string and returns the extracted parts in a new string
+ShowInConsole = Msg.replace("porro","Morro");
 ShowInConsole = Msg.replace(/porro/g,"Yoyo"); // To replace all matches, use a regular expression
 ShowInConsole = Msg.toUpperCase(); // converted to upper
 ShowInConsole = Msg.toLowerCase(); // converted to lower
@@ -25,4 +25,4 @@ ShowInConsole = Msg.charCodeAt(21); // The method returns a UTF-16 code (an inte
 ShowInConsole = Msg[21] // ECMAScript 5 (2009) allows property access [ ] on strings
 ShowInConsole = Msg.split(","); // Converting a String to an Array
 ShowInConsole = Msg.split(""); // Converting a String to an Array
-console.log(ShowInConsole)
+console.log(ShowInConsole);
