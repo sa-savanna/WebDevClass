7db6d802ae449e8e1e4c65145f33aad07551b616
injecti0n <rootgebruiker@gmail.com>
2020-03-03 12:02:17 +0100

    date object, conditions statements

--- JAVASCRIPT/testDate.js
+++ JAVASCRIPT/testDate.js
@@ -1,8 +1,13 @@
+var myTime = document.getElementById("time");
+var myBtn = document.getElementById("myBtn");
+
+myBtn.addEventListener('click',getMyTime);
 
 function getMyTime(){
     var date = new Date();
     setTimeout(getMyTime,300);
-    console.log(date.toLocaleString())
+    myTime.innerHTML=date.toLocaleString();
+    // console.log(date.toLocaleString())
 }
 
-getMyTime()
+// getMyTime()
