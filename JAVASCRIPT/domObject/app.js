65e2950f855063747b0096a9ba75a4eb14b062fc
injecti0n <rootgebruiker@gmail.com>
2020-03-09 13:56:39 +0100

    asd

--- JAVASCRIPT/domObject/app.js
+++ JAVASCRIPT/domObject/app.js
@@ -19,12 +19,51 @@ console.log(myApp.classList.length) // how many classes element uses
 
 for(var i=0; i<myApp.classList.length; i++){
 
-    console.log(myApp.classList[i]) // myApp.classList[1] ,2 ,3 
+    // console.log(myApp.classList[i]) // myApp.classList[1] ,2 ,3 
 }
+
+
+
 // myApp.innerHTML=message;
 
 // find type of html element
-console.log(h1.nodeName); // result: H1
-console.log(myApp.nodeName); // result: DIV
+    // console.log(h1.nodeName); // result: H1
+    // console.log(myApp.nodeName); // result: DIV
 
-// find all childNodes and iterate elements
+// Finding HTML Elements
+    // document.getElementById(idName);
+    // document.getElementByTagName(TagName);
+    // document.getElementsByClassName(ClassName);
+
+var FindElement = document.getElementsByTagName('h1');
+// console.log(FindElement); // returned a HTML collection as object
+
+// outerHTML property 
+    //FindElement[0].outerHTML="<p>hello</p>"
+
+// Find Element by Class Name
+
+var FindMyElementByClassName = document.getElementsByClassName('test');
+// console.log(FindMyElementByClassName)
+
+// By ClassName By TagName By ID Name
+
+// Create Element;
+var newParapgrah = document.createElement('p');
+newParapgrah.textContent="Lorem Ipsum";
+// myApp is DIV element
+    //myApp.appendChild(newParapgrah);
+
+// Remove Child
+var myList = document.getElementById('myList');
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click',removeFunction);
+
+function removeFunction(){
+    myList.removeChild(myList.firstElementChild); // remove firstchild
+    myList.removeChild(myList.lastElementChild); // remove lastchild
+    var test = document.querySelector('#myList li:nth-child(2)');
+    test.remove();
+}
+
