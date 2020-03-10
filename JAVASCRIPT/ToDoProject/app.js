7c43b57ea2cdc1aa063201ccd9b1d950ad217ebe
injecti0n <rootgebruiker@gmail.com>
2020-03-10 15:18:49 +0100

    simple todo

--- JAVASCRIPT/ToDoProject/app.js
+++ JAVASCRIPT/ToDoProject/app.js
@@ -1,6 +1,7 @@
 // UL list element
 var myUL = document.getElementById('myList');
 var myButton = document.getElementById('add');
+var tasks = document.getElementById('tasks');
 
 
 myListener();
@@ -13,7 +14,37 @@ function todoFunction(event){
     event.preventDefault();
     var input = document.createElement('input');
     var li = document.createElement('li');
+    var btn = document.createElement('button');
+    var addTodoButton = document.createElement('button');
+
+
+
+    var addTodoBtnName = document.createTextNode('Add My Task to the List')
+    addTodoButton.appendChild(addTodoBtnName);
+    // <button>TextNode</button>
+
+    btn.appendChild(document.createTextNode('X'));
     input.type='text';
     li.append(input)
+    li.appendChild(btn);
+    
+    li.appendChild(addTodoButton);
+
+
     myUL.appendChild(li);
+    
+    // add my task in to the list
+    addTodoButton.addEventListener('click', function todo(event){
+
+        tasks.innerHTML+=`
+        <li>${input.value}</li>
+        `
+        event.preventDefault();
+       
+    })
+
+    btn.addEventListener('click', function x(event) {
+        event.preventDefault();
+        myUL.removeChild(li);
+    })
 }
