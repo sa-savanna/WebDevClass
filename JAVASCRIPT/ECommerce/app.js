76df60be3bb39d28a50c6073c87cf3f084d8ef92
injecti0n <rootgebruiker@gmail.com>
2020-03-13 13:30:39 +0100

    event object

--- JAVASCRIPT/ECommerce/app.js
+++ JAVASCRIPT/ECommerce/app.js
@@ -1,1 +1,35 @@
 // PROGRAMMING HERE!!
+
+
+
+const secondBody = document.querySelectorAll("#productList")[0];
+console.log(secondBody)
+secondBody.addEventListener('click', testFunction);
+
+function testFunction(event){
+    if(event.target.id="delete"){
+        console.log("delete")
+
+
+        console.log(event.target.parentElement.parentElement.childNodes[2].nextElementSibling.outerText)
+    }
+
+    console.log(event.target.parentElement.parentElement.childNodes[2])
+}
+
+
+
+
+function MyCreateElementFunction(tagName){
+    return document.createElement(tagName)
+}
+
+var myDiv = MyCreateElementFunction('div');
+var myP = MyCreateElementFunction('p');
+var tr = MyCreateElementFunction('tr');
+var td = MyCreateElementFunction('td');
+
+// console.log(myDiv)
+// console.log(myP)
+// console.log(tr)
+// console.log(td)
