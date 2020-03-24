beda76f211928efad3af29450197f3f0c488d940
injecti0n <rootgebruiker@gmail.com>
2020-03-24 10:28:33 +0100

    updated

--- JAVASCRIPT/WebStorage/SessionStorage/LoginForm/js/register.js
+++ JAVASCRIPT/WebStorage/SessionStorage/LoginForm/js/register.js
@@ -1,7 +1,47 @@
 // html elements
-const username = document.querySelector('#username');
-const password = document.querySelector('#password');
+const inputs = document.querySelectorAll('input');
 const register = document.querySelector('#register');
+const form = document.querySelector('.login-form')
+const button = document.querySelectorAll('button');
+
+register.addEventListener('click', registerAccount);
+
+function registerAccount(event) {
+    if (typeof (Storage) !== 'undefined') {
+        if (sessionStorage.user == null && sessionStorage.pass == null && inputs[0].value !="" && inputs[1].value !="") {
+            
+            displayMessage('success',"Your account has been created");
+            sessionStorage.user = inputs[0].value;
+            sessionStorage.pass = inputs[1].value;
+            setTimeout(function(){
+                location.href='login.html';
+            },3000);
+        }else{
+            displayMessage('danger',"Inputs are empty or an account is exist in sessionStorage");
+        }
+    }
+    event.preventDefault();
+}
 
 
-register.addEventListener('click')
+button[1].addEventListener('click',logoutFunction);
+
+function logoutFunction(event) {
+    console.log('hello');
+    sessionStorage.clear();
+    event.preventDefault();
+}
+
+
+function displayMessage(type, message) {
+    const div = document.createElement('div')
+    div.className = `alert alert-${type}`;
+    div.role = "alert";
+    div.innerHTML = message;
+    form.appendChild(div);
+    console.log(div);
+    setTimeout(function () {
+        div.remove();
+    }, 2000);
+
+}
